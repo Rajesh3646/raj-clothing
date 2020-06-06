2decb9f3e2d20fd902e976a25c961724eed78dc0
rajesh.tatikonda <rajesh3646@gmail.com>
2020-06-06 23:46:45 +0530

    redux-persist on cart is completed

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux';
-import rootStore from './redux/store'
+import {store, persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react'
  
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={rootStore}>
-     <BrowserRouter><App /></BrowserRouter>
+    <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter><App /></BrowserRouter>
+    </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import {logger} from 'redux-logger';
 import {createStore, applyMiddleware} from 'redux';
-
+import {persistStore} from 'redux-persist';
 import rootReducer from './root-reducer'
 
 const middlewares = [logger];
 
-const rootStore = createStore(rootReducer, applyMiddleware(...middlewares));
-
-export default rootStore;
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
+export default {store, persistor};
